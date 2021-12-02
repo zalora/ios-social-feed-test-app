4c72787edcddf5ef9dcd37a805947e6610c072e1
Managam Silalahi <managam.silalahi@gmail.com>
2021-12-02 15:29:34 +0800

    Initial commit

---
+++ Social Feed Test App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Feed Test App
+//
+//  Created by ZALORA_USER on 02/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
