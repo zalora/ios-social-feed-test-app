35bd1503135c652afcebd275fceefb44f2a11ece
Managam Silalahi <managam.silalahi@gmail.com>
2021-12-10 12:19:16 +0800

    Tested SmartInvites with Referral but the app has to be available at least on TestFlight, so we can check the referral data

--- Social Feed Test App/ViewController.swift
+++ Social Feed Test App/ViewController.swift
@@ -10,6 +10,15 @@ import GetSocialUI
 import GetSocialSDK
 
 class ViewController: UIViewController {
+    
+    lazy var inviteFriendsButton: UIButton = {
+        let inviteFriendsButton = UIButton()
+        inviteFriendsButton.setTitle("Invite Friends", for: .normal)
+        inviteFriendsButton.setTitleColor(.systemBlue, for: .normal)
+        inviteFriendsButton.addTarget(self, action: #selector(inviteFriendsButtonTapped), for: .touchUpInside)
+        inviteFriendsButton.isEnabled = false
+        return inviteFriendsButton
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,9 +26,78 @@ class ViewController: UIViewController {
 //        setupGetSocialUI()
 //        setupGetSocialWithoutUI()
 //        isGetSocialInitialized()
+        setupView()
         listenGetSocialInitialized()
     }
     
+    private func setupView() {
+        view.addSubview(inviteFriendsButton)
+        inviteFriendsButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            inviteFriendsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            inviteFriendsButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func listenGetSocialInitialized() {
+        GetSocial.addOnInitializedListener {
+            print("GetSocial is ready to be used")
+            self.retrieveReferralDataIfAny()
+            self.inviteFriendsButton.isEnabled = GetSocial.isInitialized()
+//            self.setupGetSocialUI()
+//            self.setupGetSocialWithoutUI()
+////            self.setupGetSocialWithoutUI(onChannel: InviteChannelIds.facebook)
+//            self.isGetSocialInitialized()
+        }
+    }
+    
+    func retrieveReferralDataIfAny() {
+        Invites.setOnReferralDataReceivedListener { referralData in
+            let navigationPath = referralData.linkParams["$token"] // predefined key
+            let customValue1 = referralData.linkParams["custom_key_1"] // custom key
+            let isGuranteedMatch: Bool = referralData.isGuaranteedMatch // added metadata
+
+            print("App started with referral data: \(referralData)")
+        }
+    }
+    
+    @objc private func inviteFriendsButtonTapped() {
+        print("inviteFriendsButtonTapped")
+        
+        // Attach link params
+        let linkParams = ["country": "ID"] as [String: NSObject]
+//        LinkParams.customTitle = "Custom landing page title"
+        
+        // Customize invite message content
+        let inviteContent = InviteContent()
+        inviteContent.text = "I can't stop playing! Get it here \n\n [APP_INVITE_URL]"
+        inviteContent.subject = "Checkout this app"
+        inviteContent.mediaAttachment = MediaAttachment.imageUrl("https://docs.getsocial.im/images/logo.png")
+        inviteContent.linkParams = linkParams
+        
+        //        GetSocial.addOnInitializedListener {
+        //            // On simulator, only available native share and facebook
+        //            Invites.availableChannels { (channels) in
+        //                print(channels)
+        //            } failure: { (error) in
+        //                print(error)
+        //            }
+        //        }
+                
+        let channel = InviteChannelIds.nativeShare // or InviteChannelIds.facebook
+        
+        Invites.send(inviteContent, onChannel: channel, success: {
+            print("Invitation via \(channel) was sent")
+        }, cancel: {
+            print("Invitation via \(channel) was cancelled")
+        }, failure: { error in
+            print("Failed to send invitation via \(channel) failed, error: \(error)")
+        })
+        
+    }
+}
+
+extension ViewController {
     private func setupGetSocialUI() {
         let isShown = GetSocialUIInvitesView().show()
         print("Get Social UI Invites View is \(isShown)")
@@ -45,16 +123,6 @@ class ViewController: UIViewController {
             print("Use get GetSocial")
         } else {
             print("Cannot use GetSocial")
-        }
-    }
-    
-    private func listenGetSocialInitialized() {
-        GetSocial.addOnInitializedListener {
-            print("GetSocial is ready to be used")
-//            self.setupGetSocialUI()
-//            self.setupGetSocialWithoutUI()
-//            self.setupGetSocialWithoutUI(onChannel: InviteChannelIds.facebook)
-            self.isGetSocialInitialized()
         }
     }
 }
